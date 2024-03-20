ed00fbad6d7cec8550a578738e7681a4df570225
Vinayak Pawar <69239457+vinghost@users.noreply.github.com>
2024-03-20 17:49:33 +0530

    meme.py

--- meme.py
+++ meme.py
@@ -21,5 +21,5 @@ def index():
     memes = get_memes()
     return render_template('meme.html', memes=memes)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
